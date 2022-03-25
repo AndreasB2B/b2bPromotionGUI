c730b4b1761d4261f616d3d087bef04d96aa3a8b
AndreasB2B <andreas@b2bpromotion.dk>
2022-03-25 16:11:01 +0100

    upload

--- KN.B2B.Web/Pages/Private/Requests/Products/viewAll.cshtml.cs
+++ KN.B2B.Web/Pages/Private/Requests/Products/viewAll.cshtml.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,5 +14,31 @@ namespace KN.B2B.Web.Pages.Private.Requests.Products
         public void OnGet()
         {
         }
+
+        public void getMNData()
+        {
+            HttpWebRequest WebReqDk = (HttpWebRequest)WebRequest.Create(string.Format($"https://api.midocean.com/gateway/products/2.0?language=da"));
+
+            WebReqDk.Method = "GET";
+            WebReqDk.Headers.Add("x-gateway-APIKey", "538d5726-fc8e-4917-9d1e-0c6e2c7fe205")
+            WebReqDk.Credentials = new NetworkCredential("Casper@b2bpromotion.eu", "123456");
+            HttpWebResponse WebRespDk = (HttpWebResponse)WebReqDk.GetResponse();
+
+            string jsonStringDk;
+            using (Stream stream = WebRespDk.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8);
+
+                jsonStringDk = reader.ReadToEnd();
+            }
+
+            var settings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                MissingMemberHandling = MissingMemberHandling.Ignore
+            };
+        }
+
+
     }
 }
